d599c0718a5f78619743ebe6b502b2a14246728a
Ankit Singh <ankitsinghsingh61268@gmail.com>
2025-08-23 14:04:44 +0530

    Add File

---
+++ AgriTradeHub/AgriTradeHub/src/main/java/com/project/AgriTradeHub/AgriTradeHubApplication.java
@@ -0,0 +1,13 @@
+package com.project.AgriTradeHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriTradeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriTradeHubApplication.class, args);
+	}
+
+}
